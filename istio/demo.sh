67ab804d7c3842064c5a014db6235b5f708f093f
Chakradhar Rao Jonagam <9chakri+ds@gmail.com>
2017-06-04 21:17:10 -0700

    update

--- istio/demo.sh
+++ istio/demo.sh
@@ -2,16 +2,29 @@
 
 . $(dirname ${BASH_SOURCE})/../util.sh
 
+backtotop
+
+desc 'Run as cluster administrator'
+run 'oc login -u system:admin'
+
+backtotop
+
 desc 'Install Istio'
 
 run 'oc project default'
 run 'oc adm policy add-scc-to-user anyuid  -z default'
 run 'oc adm policy add-scc-to-user privileged -z default'
-run "oc patch scc/privileged --patch '{"allowedCapabilities":["NET_ADMIN"]}'"
+run "oc patch scc/privileged --patch '{\"allowedCapabilities\":[\"NET_ADMIN\"]}'"
 
-desc "Install Istio Service Mesh"
-run 'git clone https://github.com/istio/istio .'
-run 'oc apply -f install/kubernetes/istio.yaml'
+backtotop
+
+desc 'Install Istio Service Mesh'
+run 'git clone https://github.com/istio/istio'
+run 'oc apply -f istio/install/kubernetes/istio.yaml'
+
+backtotop
+
+desc 'Apply necessary permissions '
 
 run 'oc adm policy add-cluster-role-to-user cluster-admin -z istio-manager-service-account'
 run 'oc adm policy add-cluster-role-to-user cluster-admin -z istio-ingress-service-account'
@@ -20,23 +33,30 @@ run 'oc adm policy add-cluster-role-to-user cluster-admin  -z default'
 run 'oc adm policy add-scc-to-user anyuid  -z istio-ingress-service-account'
 run 'oc adm policy add-scc-to-user privileged -z istio-ingress-service-account'
 
-
 run 'oc adm policy add-scc-to-user anyuid  -z istio-manager-service-account'
 run 'oc adm policy add-scc-to-user privileged -z istio-manager-service-account'
 
+backtotop
 
 desc 'Isntall addons'
-run 'oc apply -f install/kubernetes/addons/prometheus.yaml'
-run 'oc apply -f install/kubernetes/addons/grafana.yaml'
-run 'oc apply -f install/kubernetes/addons/servicegraph.yamli'
+run 'oc apply -f istio/install/kubernetes/addons/prometheus.yaml'
+run 'oc apply -f istio/install/kubernetes/addons/grafana.yaml'
+run 'oc apply -f istio/install/kubernetes/addons/servicegraph.yaml'
 
+backtotop
 
 desc 'Deploy sample app'
-run 'oc apply -f <(istioctl kube-inject  -f samples/apps/bookinfo/bookinfo.yaml)'
+desc 'Install istioctl first'
+desc 'curl -L https://git.io/getIstio | sh -'
+desc 'export PATH="$PATH:/Users/jjonagam/istio/istio-0.1.5/bin"'
 
+backtotop
+desc 'Deploy bookInfo app'
+run 'oc apply -f <(istioctl kube-inject  -f istio/samples/apps/bookinfo/bookinfo.yaml)'
+run 'oc expose svc servicegraph'
 
-oc new-project mycliproject-christianh814 --description="My CLI Project" --display-name="CLI Project"
+backtotop
+desc 'Test service mesh / using grafana pod (it can be another pod)'
+run 'export GRAFANA=$(oc get pods -l app=grafana -o jsonpath={.items[0].metadata.name})'
+run 'oc exec $GRAFANA -- curl -o /dev/null -s -w "%{http_code}\n" http://istio-ingress/productpage'
 
-curl -o /dev/null -s -w "%{http_code}\n" http://istio-ingress/productpage
-
-```
